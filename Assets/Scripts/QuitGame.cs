bd600b843f5dc5eefe03127788f06e05acfb9901
Sasha <sasha.njini@hotmail.com>
2020-06-20 01:49:01 -0400

    Game

---
+++ Assets/Scripts/QuitGame.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class QuitGame : MonoBehaviour
+{
+   
+
+    void Update()
+    {
+        
+      if (Input.GetKeyDown(KeyCode.Q))
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+    }
+}
